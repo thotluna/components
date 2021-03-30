c8af73362b32b41788791558a2265c0d3ef5d016
Eladio Feijoo <eladio.feijoo@gmal.com>
2021-03-30 17:44:36 -0400

    test(create-components): intent create test create-components

--- scripts/watch-token.js
+++
@@ -1,9 +0,0 @@
-const buildTokens = require('./build-token')
-var watch = require('node-watch')
-
-watch('token/index.js', { recursive: true }, function (evt, name) {
-  console.log(`>>>> name:`, name)
-  if (evt === 'update') {
-    buildTokens('styles/tokens.css')
-  }
-})
