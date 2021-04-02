772d3dff28f315a2bd3b71c79f46668b37236cc9
Eladio Feijoo <eladio.feijoo@gmal.com>
2021-04-02 12:28:20 -0400

    feat(index): add Input at index
    feat: #19

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ export { default as Spacer } from './layout/Spacer'
 export { default as Headings } from './atoms/Headings'
 export { default as SubHeadings } from './atoms/SubHeadings'
 export { default as Icon } from './atoms/Icon'
+export { default as Input } from './atoms/Input'
 
 //molelules
 export { default as Button } from './molecules/Button'
