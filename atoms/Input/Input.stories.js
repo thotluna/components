4eca3c5d61069adc0ed87bf2a48a1a77a7d05e23
Eladio Feijoo <eladio.feijoo@gmal.com>
2021-04-02 12:11:33 -0400

    feat(input): create Input component
    feat: #19

---
+++ atoms/Input/Input.stories.js
@@ -0,0 +1,31 @@
+import { Input, options, styles } from '.'
+import {
+  getListTemplate,
+  getTemplate,
+  getOptionsArgTypes,
+} from '../../helpers/storybook'
+
+const Template = getTemplate(Input, styles)
+const ListTemplate = getListTemplate(Input, styles)
+
+export default {
+  title: 'Atoms/Input',
+  component: Input,
+  args: {
+    placeholder: 'Kingdom slowed',
+  },
+  argTypes: {
+    type: getOptionsArgTypes(options.types),
+  },
+}
+
+export const Default = Template.bind({})
+
+export const Value = Template.bind({})
+Value.args = { value: 'Others mainly cakehole Thorin moldy facial.' }
+
+export const Types = ListTemplate.bind({})
+Types.args = { items: options.types.map((type) => ({ type })) }
+
+export const Inline = Template.bind({})
+Inline.args = { isInline: true }
---
+++ atoms/Input/index.js
@@ -0,0 +1,3 @@
+export { default, Input } from './Input'
+export { options } from './constants'
+export { default as styles } from './Input.module.css'
