3704d279d1fb1117b8394748fb916337b6f55bd1
Eladio Feijoo <eladio.feijoo@gmal.com>
2021-04-03 16:26:38 -0400

    feat(card): create component Card
    A new card component was created with its respective snapshot and accessibility tests
    
    feat: #25

---
+++ atoms/Card/Card.stories.js
@@ -0,0 +1,49 @@
+import { Card, options, styles } from '.'
+import {
+  getTemplate,
+  getListTemplate,
+  getOptionsArgTypes,
+} from '../../helpers/storybook'
+
+const Template = getTemplate(Card, styles)
+const ListTemplate = getListTemplate(Card, styles)
+
+export default {
+  title: 'Atoms/Card',
+  component: Card,
+  args: {
+    children:
+      'Lacerations coaster sort comings windlance happily EIf-witch handful unbefitting? Decide rising startled Aragorn invitations midnight deserves fortunes innards. You cannot hide. I see you. There is no life in the void. Only death. Mirror Emyn dreamed!',
+  },
+  argTypes: {
+    color: getOptionsArgTypes(options.colors),
+    size: getOptionsArgTypes(options.sizes),
+    children: { control: 'text' },
+  },
+}
+
+const args = {
+  children:
+    'Lacerations coaster sort comings windlance happily EIf-witch handful unbefitting? Decide rising startled Aragorn invitations midnight deserves fortunes innards. You cannot hide. I see you. There is no life in the void. Only death. Mirror Emyn dreamed!',
+}
+
+export const Default = Template.bind({})
+Default.args = { ...args }
+
+export const Clickable = Template.bind({})
+Clickable.args = {
+  ...args,
+  isClickable: true,
+}
+
+export const Dragabble = Template.bind({})
+Dragabble.args = {
+  ...args,
+  isDraggable: true,
+}
+
+export const Colors = ListTemplate.bind({})
+Colors.args = { items: options.colors.map((color) => ({ ...args, color })) }
+
+export const Sizes = ListTemplate.bind({})
+Sizes.args = { items: options.sizes.map((size) => ({ ...args, size })) }
--- index.js
+++ index.js
@@ -2,10 +2,16 @@
 export { default as Spacer } from './layout/Spacer'
 
 //atoms
+export { default as Avatar } from './atoms/Avatar'
 export { default as Headings } from './atoms/Headings'
 export { default as SubHeadings } from './atoms/SubHeadings'
 export { default as Icon } from './atoms/Icons'
 export { default as Input } from './atoms/Input'
+export { default as Paragraph } from './atoms/Paragraph'
+export { default as Select } from './atoms/Select'
+export { default as SubHeading } from './atoms/SubHeadings'
 
 //molelules
 export { default as Button } from './molecules/Button'
+export { default as AvatarHeader } from './molecules/AvatarHeader'
+export { default as Toolbar } from './molecules/Toolbar'
