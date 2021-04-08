bd68a57babedb14211bb16a7c37089e8580ac61f
Eladio Feijoo <eladio.feijoo@gmal.com>
2021-04-08 12:47:41 -0400

    feat(container): container
    add layout container component
    
    feat: #43

---
+++ layout/Container/Container.stories.js
@@ -0,0 +1,34 @@
+/* eslint-disable react/no-unescaped-entities */
+import React from 'react'
+import Container, { options } from '.'
+import { getOptionsArgTypes, getVariusTemplate } from '../../helpers/storybook'
+
+const Template = (args) => <Container {...args} />
+const VariusTemplage = getVariusTemplate(Container)
+
+export default {
+  title: 'Layout/Container',
+  component: Container,
+  args: {
+    children: (
+      <p style={{ color: '#ffffff', fontSize: '18px' }}>
+        I can't remember anything Can't tell if this is true or dream Deep down
+        inside I feel the scream This terrible silence stops in me Now that the
+        war is through with me I'm waking up, I cannot see That there is not
+        much left of me Nothing is real but pain now Hold my breath as I wish
+        for death Oh please, God, wake me
+      </p>
+    ),
+    isPlayground: true,
+    type: 'full',
+  },
+  argTypes: {
+    children: { control: 'text' },
+    type: getOptionsArgTypes(options.types),
+  },
+}
+
+export const Default = Template.bind({})
+
+export const Types = VariusTemplage.bind({})
+Types.args = { items: options.types.map((type) => ({ type })) }
---
+++ layout/Container/index.js
@@ -0,0 +1,3 @@
+export { default, Container } from './Container'
+export { options } from './constants'
+export { default as styles } from './Container.module.css'
