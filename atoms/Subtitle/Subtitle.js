25317a8757d5279ed5e659801cbd88247e30f703
Eladio Feijoo <eladio.feijoo@gmal.com>
2021-04-08 19:03:28 -0400

    feat(subtitle): add Subtite component
    "feat: #45"

---
+++ atoms/Subtitle/Subtitle.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { styles, options } from '.'
+import { getClasses } from '../../helpers/styles'
+
+export const Subtitle = ({ children, color, size }) => {
+  const getStyles = getClasses(styles)({ color })
+  return (
+    <>
+      {size === 'lg' && (
+        <h3 className={getStyles('container', ['color'])}>{children}</h3>
+      )}
+      {size === 'md' && (
+        <h4 className={getStyles('container', ['color'])}>{children}</h4>
+      )}
+      {size === 'sm' && (
+        <h5 className={getStyles('container', ['color'])}>{children}</h5>
+      )}
+    </>
+  )
+}
+
+Subtitle.propTypes = {
+  children: PropTypes.node,
+  color: PropTypes.oneOf(options.colors),
+  size: PropTypes.oneOf(options.sizes),
+}
+
+Subtitle.defaultProps = {
+  size: 'md',
+  color: 'base',
+}
+
+export default Subtitle
---
+++ atoms/Subtitle/index.js
@@ -0,0 +1,3 @@
+export { default, Subtitle } from './Subtitle'
+export { options } from './constants'
+export { default as styles } from './Subtitle.module.css'
